da66fea6a1f0490c2569343b54ba21b3c0130823
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-17 18:12:50 +0530

    Enhance book search functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,10 @@ class BooksController < ApplicationController
     @books = Book.includes(:authors, :publisher, :subjects)
                  .order(:title)
                  .page(params[:page])
-                 .per(12)  
+                 .per(12)
     @total_books = Book.count
+    
+    @subjects = Subject.order(:name)
   end
 
   def show
@@ -13,16 +15,30 @@ class BooksController < ApplicationController
   end
 
   def search
-    if params[:query].present?
-      @books = Book.where("title LIKE ?", "%#{params[:query]}%")
-                   .includes(:authors, :publisher)
-                   .order(:title)
-                   .page(params[:page])
-                   .per(12)
-      @total_books = Book.where("title LIKE ?", "%#{params[:query]}%").count
+    @subjects = Subject.order(:name)
+    @selected_subject = params[:subject_id].present? ? Subject.find(params[:subject_id]) : nil
+    
+    if params[:query].present? || params[:subject_id].present?
+      @books = Book.includes(:authors, :publisher, :subjects)
+      
+      if params[:query].present?
+        @books = @books.where(
+          "title LIKE ? OR EXISTS (SELECT 1 FROM book_authors ba JOIN authors a ON ba.author_id = a.id WHERE ba.book_id = books.id AND a.name LIKE ?)",
+          "%#{params[:query]}%", "%#{params[:query]}%"
+        )
+      end
+      
+      if params[:subject_id].present?
+        @books = @books.joins(:subjects).where(subjects: { id: params[:subject_id] })
+      end
+      
+      @books = @books.order(:title).page(params[:page]).per(12)
+      @total_books = @books.count
+      @search_performed = true
     else
       redirect_to books_path
     end
+    
     render :index
   end
 end
